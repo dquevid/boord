536f2a90c8656c66493d3fb6da69d7a254209592
dqvid <david.lord.ru@gmail.com>
2024-05-29 01:30:18 +0300

    0.34: Slight manifest edit

--- src/app/entry.jsx
+++ src/app/entry.jsx
@@ -22,14 +22,14 @@ const router = createBrowserRouter([
             {
                 path: '/boord/*',
                 element: (
-                    <div className="flex flex-col items-baseline gap-3 p-8">
-                        <h1 className="text-3xl text-neutral-900 font-bold before:">
+                    <div className="flex flex-col items-baseline gap-4 p-8">
+                        <h1 className="text-3xl text-neutral-900 font-bold relative before:absolute before:content-[''] before:left-[-1rem] before:top-[50%] before:translate-y-[-50%] before:w-1.5 before:h-[50%] before:rounded-full before:bg-red-400">
                             404 — Page not found.
                         </h1>
                         <p className="text-neutral-600 leading-6">
                             К сожалению, нам не удалось найти запрашиваемую страницу.
                         </p>
-                        <Link to="/boord" className="rounded-md text-neutral-100 bg-neutral-900 px-4 py-2">
+                        <Link to="/boord" className="rounded-lg text-neutral-100 bg-neutral-900 px-4 py-2">
                             Вернуться
                         </Link>
                     </div>
